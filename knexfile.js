c19f268474abcc6b8ab0a1ccfd3c78d9927fd219
Jefferson00 <jeffersonts00@gmail.com>
2020-07-12 15:11:06 -0300

    variavel ambiente BD

--- knexfile.js
+++ knexfile.js
@@ -21,17 +21,17 @@ module.exports = {
 
   staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
     }
   },
 
